a29d0e065eaee6f8a21a559f5c21b966fb132ffa
Riza Nafis <riza.nafis@gmail.com>
2024-06-09 20:14:51 +0700

    chore: remove old neo tree conf

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',

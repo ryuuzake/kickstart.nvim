7f85b1071d476ea276b4908edfbae8ac6a29c679
Riza Nafis <riza.nafis@dana.id>
2024-10-18 09:37:35 +0700

    add copilot config

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,24 @@
+vim.g.copilot_assume_mapped = true
+
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true, -- overrides default
+        terraform = false, -- disallow specific filetype
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
+      },
+    }
+  end,
+}
